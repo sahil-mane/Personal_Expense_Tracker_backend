8fde0fed20b912e07a19f97046390d76939d949d
sahil-mane <manesahil238@gmail.com>
2026-07-14 01:19:36 +0530

    update

---
+++ app.js
@@ -0,0 +1,40 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import connectDatabase from "./db.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
+
+const app = express();
+
+connectDatabase();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Expense Tracker API is running 🚀",
+  });
+});
+
+app.use("/api/expenses", expenseRoutes);
+console.log(process.env.PORT)
+
+app.use((error, req, res, next) => {
+  if (error.name === "CastError") {
+    return res.status(400).json({ message: "Invalid expense ID." });
+  }
+
+  if (error.name === "ValidationError") {
+    return res.status(400).json({ message: error.message });
+  }
+
+  console.error(error);
+
+  res.status(500).json({
+    message: "Something went wrong.",
+  });
+});
+
+export default app;
--- index.js
+++ index.js
@@ -1,39 +1,20 @@
 import "dotenv/config";
-import cors from "cors";
-import express from "express";
+import app from "./app.js";
 import connectDatabase from "./db.js";
-import expenseRoutes from "./routes/expenseRoutes.js";
 
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-connectDatabase();
+async function startServer() {
+  try {
+    await connectDatabase();
 
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Expense Tracker API is running 🚀",
-  });
-});
-
-app.use("/api/expenses", expenseRoutes);
-
-app.use((error, req, res, next) => {
-  if (error.name === "CastError") {
-    return res.status(400).json({ message: "Invalid expense ID." });
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
   }
+}
 
-  if (error.name === "ValidationError") {
-    return res.status(400).json({ message: error.message });
-  }
-
-  console.error(error);
-
-  res.status(500).json({
-    message: "Something went wrong.",
-  });
-});
-
-export default app;
+startServer();
